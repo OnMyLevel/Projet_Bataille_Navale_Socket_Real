1a4f89cf75e92ef5dd0cf8d77cb1fad0a375bc16
christanismerilbanzouzi <merilb78@gmail.com>
2018-09-17 22:58:11 +0200

    Fin des méthodes de placement

--- src/model/Croiseur.java
+++ src/model/Croiseur.java
@@ -1,11 +1,14 @@
 package model;
 
 public class Croiseur extends Bateau {
+
     /** Creates a new instance of Croiseur */
     public Croiseur(int x, int y, int taille,boolean vertical) {
+
         super(x,y, taille, vertical);
     }
     public String toString(){
+
         return "croiseur"+super.toString();
     }
 }
